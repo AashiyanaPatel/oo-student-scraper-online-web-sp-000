6d79da5d7af46202cc9ee8be34819d014d95e9d1
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-03-27 00:57:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    #binding.pry
+    
     @name = student_hash[:name]
     @location = student_hash[:location]
     student_hash = self
